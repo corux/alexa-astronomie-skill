1b6e127f4f2b61554da4d4aff30fefb2f7680e64
Tobias Wallura <tobias@wallura.org>
2017-10-14 11:57:41 +0200

    include only first element in rss feed

--- src/flash-briefing.js
+++ src/flash-briefing.js
@@ -4,14 +4,13 @@ const url = 'https://www.astronomie.info/news/rss-bulletin.xml';
 
 module.exports = (event, context, callback) => {
     feedparser.parse(url).then((items) => {
-        items.forEach(item => {
-            callback(null, {
-                uid: item.guid,
-                updateDate: item.pubdate,
-                titleText: item.title,
-                mainText: item.description,
-                redirectionUrl: item.link
-            });        
+        let item = items[0];
+        callback(null, {
+            uid: item.guid,
+            updateDate: item.pubdate,
+            titleText: item.title,
+            mainText: item.description,
+            redirectionUrl: item.link
         });
     });
 };
