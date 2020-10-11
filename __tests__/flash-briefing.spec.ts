446710703103bdb1ac8325ce9e94305b3efa67d0
Tobias Wallura <tobias@wallura.org>
2020-10-11 19:24:13 +0200

    disable skill due to retired astronomie info

--- __tests__/flash-briefing.spec.ts
+++ __tests__/flash-briefing.spec.ts
@@ -1,12 +1,8 @@
 import { handler } from "../src/flash-briefing";
 
 describe("Handler", () => {
-  it("should retrieve and parse data", async () => {
+  it("should retrieve empty array", async () => {
     const response = await handler(null, null, null);
-    expect(response.titleText).toContain("Heute am Himmel");
-    expect(response.mainText).toBeTruthy();
-    expect(response.uid).toBeTruthy();
-    expect(response.updateDate).toBeTruthy();
-    expect(response.redirectionUrl).toBe("https://news.astronomie.info/hah");
+    expect(response).toStrictEqual([]);
   });
 });
